d8499767f489ac35f8e01e7b264d95c7cbd4f855
Xavier Mirabelli-Montan <xavier@whiteskyweb.com>
2024-10-08 14:03:41 +0100

    refactor: Moving timestamp functions to a util

--- frontend/utils/utils.ts
+++ frontend/utils/utils.ts
@@ -2,3 +2,33 @@ export const isOnlyEmoji = (message: string) => {
   const trimmedMessage = message.trim();
   return trimmedMessage && /^[\p{Emoji}]+$/u.test(trimmedMessage);
 };
+
+export const handleKeyDown = (
+  event: React.KeyboardEvent<HTMLInputElement>,
+  func: () => void,
+) => {
+  if (event.key === "Enter") {
+    func();
+  }
+};
+
+export const formattedDate = (
+  date?: string,
+  options: Intl.DateTimeFormatOptions = {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  },
+) => (date ? new Date(date).toLocaleDateString("en-US", options) : "");
+
+export const formattedTime = (
+  date?: string,
+  options: Intl.DateTimeFormatOptions = {
+    hour: "numeric",
+    minute: "2-digit",
+    hour12: true,
+  },
+) => (date ? new Date(date).toLocaleTimeString("en-US", options) : "");
+
+export const dateToTimestamp = (date: string) =>
+  Math.floor(new Date(date).getTime() / 1000);
